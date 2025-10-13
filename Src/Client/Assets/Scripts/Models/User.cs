e1af72bb43445083eeec469fe728fe319e8413f7
陈清规 <kuzumi1202@163.com>
2025-10-13 16:57:35 +0800

    "用户注册登录功能完成"

---
+++ Src/Client/Assets/Scripts/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Models
+{
+    internal class User : Singleton<User>
+    {
+        private SkillBridge.Message.NUserInfo userInfo;
+
+        public SkillBridge.Message.NUserInfo Info
+        {
+            get { return userInfo; }
+        }
+
+        public void SetupUserInfo(SkillBridge.Message.NUserInfo info)
+        {
+            this.userInfo = info;
+        }
+
+        public SkillBridge.Message.NCharacterInfo CurrentCharacter { get; set; }
+    }
+}
